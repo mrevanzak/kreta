2c14ea70c4db19e3254c462c95088a5ab259a404
mrevanzak <mrevanzak@gmail.com>
2025-10-08 11:13:05 +0700

    chore: add widget target and reorg folders

---
+++ Client/Widget/index.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by Revanza Kurniawan on 08/10/25.
+//
+
+import SwiftUI
+import WidgetKit
+
+@main
+struct exportWidgets: WidgetBundle {
+  var body: some Widget {
+    WidgetLiveActivity()
+  }
+}
