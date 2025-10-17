461f0f217f32f6fddf2f551651d007ef05a4a261
mrevanzak <mrevanzak@gmail.com>
2025-10-17 10:53:41 +0700

    feat: live activities lock screen design

--- Client/Widget/WidgetLiveActivity.swift
+++ Client/Widget/WidgetLiveActivity.swift
@@ -13,25 +13,19 @@ import WidgetKit
 struct ActivityView: View {
   let context: ActivityViewContext<TrainActivityAttributes>
   var body: some View {
-    VStack(alignment: .leading, spacing: 6) {
-      HStack(spacing: 8) {
-        Text("🚆")
-        Text("\(context.attributes.from.name) → \(context.attributes.destination.name)")
-          .font(.headline)
+    VStack {
+      HStack {
+        TrainExpandedLeadingView(context: context)
+        Spacer()
+        TrainExpandedCenterView(context: context)
+        Spacer()
+        TrainExpandedTrailingView(context: context)
       }
-      Text("Next: \(context.state.stations.next.name)")
-        .font(.subheadline)
-        .foregroundStyle(.secondary)
-      HStack(spacing: 6) {
-        Image(systemName: "clock")
-        // Relative time remaining until the destination ETA
-        // Text(context.state.estimatedArrival, style: .relative)
-        //   .font(.title3).monospacedDigit()
-      }
+      TrainExpandedBottomView(context: context, padding: 24)
     }
-    .padding(.vertical, 8)
-    .activityBackgroundTint(Color(uiColor: .systemBackground))
-    .activitySystemActionForegroundColor(Color.accentColor)
+    .padding()
+    // .activityBackgroundTint(Color(uiColor: .systemBackground))
+    // .activitySystemActionForegroundColor(Color.accentColor)
   }
 }
 
@@ -73,6 +67,111 @@ struct ActivityProgressView: View {
   }
 }
 
+struct TrainExpandedBottomView: View {
+  let context: ActivityViewContext<TrainActivityAttributes>
+  var padding: CGFloat? = 0
+
+  var body: some View {
+    VStack {
+      Image("kereta")
+        .resizable()
+        .scaledToFit()
+      HStack(alignment: .center) {
+        VStack(alignment: .leading, spacing: 4) {
+          Text(context.state.stations.previous.code).font(.body).bold()
+          Text(context.state.stations.previous.name)
+            .font(.caption)
+            .foregroundStyle(.secondary)
+            .lineLimit(1)
+            .truncationMode(.tail)
+        }
+        .padding(.leading, padding)
+        .containerRelativeFrame(.horizontal) { size, _ in
+          size * 0.3
+        }
+
+        VStack(alignment: .center, spacing: 4) {
+          Text(context.attributes.train.name).font(.body).bold()
+          Text("KA\(context.attributes.train.code)").font(.caption).foregroundStyle(
+            .secondary)
+        }
+        .containerRelativeFrame(.horizontal) { size, _ in
+          size * 0.4
+        }
+
+        VStack(alignment: .trailing, spacing: 4) {
+          Text(context.state.stations.next.code).font(.body).bold()
+          Text(context.state.stations.next.name)
+            .font(.caption)
+            .foregroundStyle(.secondary)
+            .lineLimit(1)
+            .truncationMode(.tail)
+        }
+        .padding(.trailing, padding)
+        .containerRelativeFrame(.horizontal) { size, _ in
+          size * 0.3
+        }
+      }
+    }
+  }
+}
+
+struct TrainExpandedLeadingView: View {
+  let context: ActivityViewContext<TrainActivityAttributes>
+  var body: some View {
+    VStack(alignment: .leading, spacing: 4) {
+      Text(context.attributes.destination.code).font(.title3).bold()
+      Text(context.attributes.destination.name)
+        .font(.caption)
+        .foregroundStyle(.secondary)
+        .lineLimit(1)
+        .truncationMode(.tail)
+    }
+  }
+}
+
+struct TrainExpandedTrailingView: View {
+  let context: ActivityViewContext<TrainActivityAttributes>
+  var body: some View {
+    VStack(alignment: .trailing, spacing: 4) {
+      if let destinationEstimatedArrival = context.attributes.destination.estimatedArrival {
+        Text(
+          destinationEstimatedArrival,
+          format: .biggestUnitRelative(units: 2)
+        )
+        .font(.title3)
+        .bold()
+        .minimumScaleFactor(0.1)
+        .lineLimit(1)
+      }
+
+      //TODO: add logic later
+      Text("Terlambat")
+        .font(.caption).bold()
+        .padding(.horizontal, 8)
+        .padding(.vertical, 4)
+        .background(Color(.systemRed).opacity(0.2))
+        .clipShape(Capsule())
+        .foregroundColor(Color(.systemRed))
+        .minimumScaleFactor(0.1)
+        .lineLimit(1)
+    }
+  }
+}
+
+struct TrainExpandedCenterView: View {
+  let context: ActivityViewContext<TrainActivityAttributes>
+  var body: some View {
+    //TODO: add logic later
+    Text("4 menit menuju GDB")
+      .font(.caption).bold()
+      .padding(.horizontal, 8)
+      .padding(.vertical, 4)
+      .background(Color(.systemGray2).opacity(0.2))
+      .clipShape(Capsule())
+  }
+}
+
 struct WidgetLiveActivity: Widget {
   var body: some WidgetConfiguration {
     ActivityConfiguration(for: TrainActivityAttributes.self) { context in
@@ -81,96 +180,21 @@ struct WidgetLiveActivity: Widget {
       DynamicIsland {
         // Expanded regions
         DynamicIslandExpandedRegion(.leading) {
-          VStack(alignment: .leading, spacing: 4) {
-            Text(context.attributes.destination.code).font(.title3).bold()
-            Text(context.attributes.destination.name)
-              .font(.caption)
-              .foregroundStyle(.secondary)
-              .lineLimit(1)
-              .truncationMode(.tail)
-          }
-          .padding(.leading)
+          TrainExpandedLeadingView(context: context)
+            .padding(.leading)
         }
 
         DynamicIslandExpandedRegion(.trailing) {
-          VStack(alignment: .trailing, spacing: 4) {
-            if let destinationEstimatedArrival = context.attributes.destination.estimatedArrival {
-              Text(
-                destinationEstimatedArrival,
-                format: .biggestUnitRelative(units: 2)
-              )
-              .font(.title3)
-              .bold()
-              .minimumScaleFactor(0.1)
-              .lineLimit(1)
-            }
-
-            //TODO: add logic later
-            Text("Terlambat")
-              .font(.caption).bold()
-              .padding(.horizontal, 8)
-              .padding(.vertical, 4)
-              .background(Color(.systemRed).opacity(0.2))
-              .clipShape(Capsule())
-              .foregroundColor(Color(.systemRed))
-              .minimumScaleFactor(0.1)
-              .lineLimit(1)
-
-          }
-          .padding(.trailing)
+          TrainExpandedTrailingView(context: context)
+            .padding(.trailing)
         }
+
         DynamicIslandExpandedRegion(.center) {
-          //TODO: add logic later
-          Text("4 menit menuju GDB")
-            .font(.caption).bold()
-            .padding(.horizontal, 8)
-            .padding(.vertical, 4)
-            .background(Color(.systemGray2).opacity(0.2))
-            .clipShape(Capsule())
+          TrainExpandedCenterView(context: context)
         }
 
         DynamicIslandExpandedRegion(.bottom) {
-          VStack {
-            Image("kereta")
-              .resizable()
-              .scaledToFit()
-            HStack(alignment: .center) {
-              VStack(alignment: .leading, spacing: 4) {
-                Text(context.state.stations.previous.code).font(.body).bold()
-                Text(context.state.stations.previous.name)
-                  .font(.caption)
-                  .foregroundStyle(.secondary)
-                  .lineLimit(1)
-                  .truncationMode(.tail)
-              }
-              .padding(.leading, 36)
-              .containerRelativeFrame(.horizontal) { size, _ in
-                size * 0.3
-              }
-
-              VStack(alignment: .center, spacing: 4) {
-                Text(context.attributes.train.name).font(.body).bold()
-                Text("KA\(context.attributes.train.code)").font(.caption).foregroundStyle(
-                  .secondary)
-              }
-              .containerRelativeFrame(.horizontal) { size, _ in
-                size * 0.4
-              }
-
-              VStack(alignment: .trailing, spacing: 4) {
-                Text(context.state.stations.next.code).font(.body).bold()
-                Text(context.state.stations.next.name)
-                  .font(.caption)
-                  .foregroundStyle(.secondary)
-                  .lineLimit(1)
-                  .truncationMode(.tail)
-              }
-              .padding(.trailing, 36)
-              .containerRelativeFrame(.horizontal) { size, _ in
-                size * 0.3
-              }
-            }
-          }
+          TrainExpandedBottomView(context: context, padding: 36)
         }
       } compactLeading: {
         HStack(alignment: .center, spacing: 8) {
