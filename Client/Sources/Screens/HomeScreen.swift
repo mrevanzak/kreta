52acd973460d320a9608f8e3e0c3fbec128c76f1
BIru <gilangbbe@gmail.com>
2025-10-22 08:56:44 +0700

    feat: implement AddTrain view and integrate with HomeScreen

--- Client/Sources/Screens/HomeScreen.swift
+++ Client/Sources/Screens/HomeScreen.swift
@@ -1,42 +1,57 @@
+import MapKit
 import SwiftUI
 
-func getDate(from time: String) -> Date? {
-  let formatter = DateFormatter()
-  formatter.dateFormat = "yyyy/MM/dd HH:mm"
-  return formatter.date(from: time) ?? nil
-}
 
 @MainActor
 struct HomeScreen: View {
   @available(iOS 16.1, *)
   var trainLiveActivityService: TrainLiveActivityService = TrainLiveActivityService.shared
-
+    
+    @State private var showTrip = true
+    @State private var showAddTrip = false
+    
   var body: some View {
-    Button("Train Live Activity") {
-      Task { @MainActor in
-        do {
-          _ = try await trainLiveActivityService.start(
-            train: Train(name: "Jayabaya", code: "91"),
-            from: TrainStation(
-              name: "Malang", code: "ML",
-              estimatedArrival: nil,
-              estimatedDeparture: getDate(from: "2025/10/16 13:45"),
-            ),
-            destination: TrainStation(
-              name: "Pasar Senen", code: "PSE",
-              estimatedArrival: getDate(from: "2025/10/17 01:58"),
-              estimatedDeparture: nil
-            ),
-            nextStation: TrainStation(
-              name: "Surabaya Pasarturi", code: "SBI",
-              estimatedArrival: getDate(from: "2025/10/16 15:55"),
-              estimatedDeparture: getDate(from: "2025/10/16 16:07")
-            ),
-          )
-        } catch {
-          print("Failed to start train live activity: \(error)")
+    Map{}
+    .sheet(isPresented: $showTrip) {
+        VStack() {
+            HStack {
+                Text("Perjalanan Kereta")
+                    .font(.title2).bold()
+                Spacer()
+                Image(systemName: "gear.circle")
+                    .font(.system(size: 38))
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color(.green))
+            }
+            .padding(.top, 23)
+            Button {
+                showAddTrip = true
+            } label: {
+                VStack(spacing: 10) {
+                    Image(systemName: "plus")
+                        .font(.system(size: 56, weight: .semibold))
+                    Text("Tambah Perjalanan\nKereta")
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.secondary).bold()
+                }
+                .frame(maxWidth: .infinity, minHeight: 200)
+                .background(
+                    RoundedRectangle(cornerRadius: 24)
+                        .fill(.secondary)
+                )
+            }
+            .buttonStyle(.plain)
         }
-      }
+        .presentationDetents([.height(264)])
+        .padding(.horizontal, 21)
+        .presentationBackground(.white)
+        .presentationDragIndicator(.hidden)
+        .interactiveDismissDisabled(true)
+    }
+    .sheet(isPresented: $showAddTrip) {
+        AddTrain { TrainMock in
+            
+        }
     }
   }
 }
