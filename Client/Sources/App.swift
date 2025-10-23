7336e1e133709dd18e492dc45b1d1f5bcc7eedb5
mrevanzak <mrevanzak@gmail.com>
2025-10-23 09:52:37 +0700

    refactor: rename HelloMarketClientApp to KretaApp and hide tab bar toolbar

--- Client/Sources/App.swift
+++ Client/Sources/App.swift
@@ -2,7 +2,7 @@ import ConvexMobile
 import SwiftUI
 
 @main
-struct HelloMarketClientApp: App {
+struct KretaApp: App {
   @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
   @State private var convexClient = Dependencies.shared.convexClient
 
