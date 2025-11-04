eaedeb78053d7526034c89305021d30658bd9d89
mrevanzak <mrevanzak@gmail.com>
2025-11-04 23:40:26 +0700

    chore: remove unused code

--- Client/Tests/Utility/DataHexEncodingTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import kreta
-
-final class DataHexEncodingTests: XCTestCase {
-    func testHexEncodingProducesExpectedString() {
-        let data = Data([0x0f, 0xa0, 0x1b])
-        XCTAssertEqual(data.hexEncodedString(), "0fa01b")
-    }
-}
